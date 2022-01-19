6d165ca627175aae5afd3666cb4e729d15e761ac
juan carlos fedyna <jfedyna@outlook.com>
2022-01-18 23:10:38 -0300

    Modificacion app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('servidor corriendo 3000');
 });
 app.get('/', (req, res) => {
